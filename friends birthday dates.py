be43c5d3004e8101e0721785d6b9f5c23dd83b52
Adhishiyaa <bgomathiece2014@gmail.com>
2026-04-19 13:41:55 +0530

    Created using Colab

---
+++ friends birthday dates.py
@@ -0,0 +1,67 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOEnWiICsTqx7ejtPL9FFSt",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Adhishiyaa/python-codes/blob/main/friends%20birthday%20dates.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 6,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "Fa_j_0ZgtBCF",
+        "outputId": "9b67f973-f9d2-4447-9599-e825279b75ba"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "first friend's b'day date is 20\n",
+            "second friend's b'day date is 14\n",
+            "third friend's b'day date is 27\n",
+            "fourth friend's b'day date is 10\n",
+            "fifth friend's b'day date is 24\n"
+          ]
+        }
+      ],
+      "source": [
+        "friend_1=20\n",
+        "friend_2=14\n",
+        "friend_3=27\n",
+        "friend_4=10\n",
+        "friend_5=24\n",
+        "print(\"first friend's b'day date is\",friend_1)\n",
+        "print(\"second friend's b'day date is\",friend_2)\n",
+        "print(\"third friend's b'day date is\",friend_3)\n",
+        "print(\"fourth friend's b'day date is\",friend_4)\n",
+        "print(\"fifth friend's b'day date is\",friend_5)\n",
+        "\n"
+      ]
+    }
+  ]
+}
