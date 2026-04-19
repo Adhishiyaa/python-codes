a8bddb1a689f2c716ac85e45d269e57409bb3b8f
Adhishiyaa <bgomathiece2014@gmail.com>
2026-04-19 13:55:56 +0530

    Created using Colab

---
+++ congratulations.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPUFg1YmGLqjOi87hcei6++",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Adhishiyaa/python-codes/blob/main/congratulations.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "word=\"congratulations\"\n",
+        "print(word.upper())"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "glfJExoo0ENq",
+        "outputId": "874ebc00-318f-4571-e4dd-5134ef5479c8"
+      },
+      "execution_count": 7,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "CONGRATULATIONS\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
