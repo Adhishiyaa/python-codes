8ca6d78d9a1d434be0ac9ba4d2b5c233267cbae1
Adhishiyaa <bgomathiece2014@gmail.com>
2026-04-19 17:40:06 +0530

    day 6 operators

---
+++ day 6 operators/or.py
@@ -0,0 +1,13 @@
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("either of these number is greater than 0")
+else:
+    print("none of these number is greater than 0")
+
+if b>0 or c>0:
+    print("either one of these numbers is greater than 0 ")
+else:
+    print("none of these numbers are greater than 0")
+        
