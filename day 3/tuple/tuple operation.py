961aae3de48d97b5544a598f4579ece5a7a68dcd
Adhishiyaa <bgomathiece2014@gmail.com>
2026-06-21 17:47:31 +0530

    .

---
+++ day 3/tuple/tuple operation.py
@@ -0,0 +1,18 @@
+tuplex=("tuplex", False, 3.2,  1)
+print(tuplex)
+
+tuplex = (4, 5, 1, 2, 11, 7, 10, 8)
+print(tuplex)
+
+tuplex = tuplex + (12, 13, 27)
+print(tuplex)
+
+tuple1 = (74, 27, 74, 47, 19, 30, 27, 74)
+print(tuple1.count(74))
+
+tuplex = (2, 7, 4, 1, 9, 3, 0, 1, 2, 7)
+_slice = tuplex[2:6]
+print(_slice)
+
+_slice = tuplex[:7]
+print(_slice)
