9a69b4d04b0acdb4b1adfd4a96331583797760a2
Adhishiyaa <bgomathiece2014@gmail.com>
2026-09-21 18:44:15 +0530

    .

---
+++ day 3/test4/library system.py
@@ -0,0 +1,46 @@
+class book:
+    def __init__(self,title,author,price):
+        self.title=title
+        self.author=author
+        self.price=price
+        self.is_borrowed=True
+
+    def display_info(self):
+        print("Book title:", self.title)
+        print("Book author:", self.author)
+        print("Book price:", self.price)
+        print("Book is borrowed:", self.is_borrowed)
+
+return_book = book("science novel", "John Doe", 29.99)
+return_book.display_info()
+return_book=False
+def borrow_book(self):
+        if self.is_borrowed:
+            print("Sorry, this book is already borrowed.")
+        else:
+            self.is_borrowed = True
+            print("You have successfully borrowed the book.")
+is_borrowed=True
+def display_info(self):
+        print("Book title:", self.title)
+        print("Book author:", self.author)
+        print("Book price:", self.price)
+        print("Book is borrowed:", self.is_borrowed)
+return_book = book("english novel", "Rose ", 29.99)    
+return_book.display_info()
+return_book=False
+def borrow_book(self):
+        if self.is_borrowed:
+            print("Sorry, this book is already borrowed.")
+        else:
+            self.is_borrowed = True
+            print("You have successfully borrowed the book.")
+is_borrowed=True
+def display_info(self):
+        print("Book title:", self.title)
+        print("Book author:", self.author)
+        print("Book price:", self.price)
+        print("Book is borrowed:", self.is_borrowed)
+return_book = book("math novel", "Jisoo ", 29.99)
+return_book.display_info()
+return_book=False
